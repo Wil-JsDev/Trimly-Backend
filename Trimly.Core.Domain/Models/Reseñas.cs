7200804b6eeaad4a4cf38b4b197d531194dfc940
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-02-18 14:11:25 -0400

    feat: add models

---
+++ Trimly.Core.Domain/Models/Reseñas.cs
@@ -0,0 +1,17 @@
+﻿using Trimly.Core.Domain.Common;
+
+namespace Trimly.Core.Domain.Models
+{
+    public sealed class Reseñas : FechaCreacionActualizacion
+    {
+        public Guid? ReseñasId { get; set; }
+
+        public string? Titulo {  get; set; }
+
+        public string? Comentario { get; set; }
+
+        public int Calificacion {  get; set; }
+
+        public Guid? EmpresasRegistradasId { get; set; }
+    }
+}
