b7649a00cffbf6bd5ef0fb4d02e1106dc9beaf31
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-02-19 09:27:38 -0400

    chore: translated models from Spanish to English for improved consistency

---
+++ Trimly.Core.Domain/Models/Reservations.cs
@@ -0,0 +1,20 @@
+﻿using Trimly.Core.Domain.Common;
+
+namespace Trimly.Core.Domain.Models
+{
+    public sealed class Reservations : CreationDateUpdate
+    {
+        public Guid? ReservationsId { get; set; }
+
+        public DateTime? StartDateTime { get; set; }
+
+        public DateTime? EndDateTime { get; set; }
+
+        public string? Note { get; set; }
+
+        public string? ConfirmationCode { get; set; }
+
+        public Guid? AppointmentId { get; set; }
+    }
+
+}
---
+++ Trimly.Core.Domain/Models/Reviews.cs
@@ -0,0 +1,17 @@
+﻿using Trimly.Core.Domain.Common;
+
+namespace Trimly.Core.Domain.Models
+{
+    public sealed class Reviews : CreationDateUpdate
+    {
+        public Guid? ReviewId { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Comment { get; set; }
+
+        public int Rating { get; set; }
+
+        public Guid? RegisteredCompanyId { get; set; }
+    }
+}
