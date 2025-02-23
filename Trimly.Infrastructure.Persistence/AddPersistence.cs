091921953c2e9b98a4b4b9fb0e418a671a0de24a
Dayron <dayronbp06@gmail.com>
2025-02-23 16:38:04 -0400

    git add persistence layer

--- Trimly.Infrastructure.Persistence/AddPersistence.cs
+++ Trimly.Infrastructure.Persistence/AddPersistence.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Trimly.Infrastructure.Persistence.Context;
 
 namespace Trimly.Infrastructure.Persistence
 {
-    public class AddPersistence
+    public static class AddPersistence
     {
+        public static void AddPersistenceMethod(this IServiceCollection Services, IConfiguration Configuration)
+        {
+            #region Connection
+
+            Services.AddDbContext<TrimlyContext>(b =>
+            {
+                b.UseSqlServer(Configuration.GetConnectionString("TrimlyConnection"),
+                    c => c.MigrationsAssembly(typeof(TrimlyContext).Assembly.FullName));
+            });
+
+            #endregion
+        }
+
     }
 }
