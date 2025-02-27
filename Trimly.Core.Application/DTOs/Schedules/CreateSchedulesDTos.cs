3fd399b4712adaa53f443439c039d5471f6d7d5f
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-02-26 22:32:22 -0400

    feat: add schedules dtos

---
+++ Trimly.Core.Application/DTOs/Schedules/CreateSchedulesDTos.cs
@@ -0,0 +1,13 @@
+﻿using Trimly.Core.Domain.Enum;
+namespace Trimly.Core.Application.DTOs.Schedules
+{
+    public sealed record CreateSchedulesDTos
+    (
+        Weekday? Days,
+        TimeOnly OpeningTime,
+        TimeOnly ClosingTime,
+        string? Notes,
+        Status? IsHolady,
+        Guid? RegisteredCompanyId
+    );
+}
---
+++ Trimly.Core.Application/DTOs/Schedules/UpdateSchedulesDTos.cs
@@ -0,0 +1,12 @@
+﻿using Trimly.Core.Domain.Enum;
+namespace Trimly.Core.Application.DTOs.Schedules
+{
+    public sealed record UpdateSchedulesDTos
+    (
+        Weekday? Days,
+        TimeOnly OpeningTime,
+        TimeOnly ClosingTime,
+        string? Notes,
+        Status? IsHolady
+    ); 
+}
