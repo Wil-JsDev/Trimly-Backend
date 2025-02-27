c98ab029137177b741c8d285750fe60c64a20a61
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-02-26 22:32:01 -0400

    feat: add reviews dtos

---
+++ Trimly.Core.Application/DTOs/Review/CreateReviewsDTos.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Trimly.Core.Application.DTOs.Review
+{
+    public sealed record CreateReviewsDTos
+    (
+        string? Title,
+        string? Comment,
+        int Rating
+    );
+}
---
+++ Trimly.Core.Application/DTOs/Review/ReviewsDTos.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Trimly.Core.Application.DTOs.Review
+{
+    public sealed record ReviewsDTos
+    (
+        Guid? ReviewId,
+        string? Title,
+        string? Comment,
+        int Rating,
+        Guid? RegisteredCompanyId,
+        DateTime? CreatedAt,
+        DateTime? UpdateAt
+    );
+}
