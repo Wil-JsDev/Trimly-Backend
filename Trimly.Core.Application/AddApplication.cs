cdfae7e201be03fccc4d4cec051d708302232221
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-02-26 22:35:07 -0400

    feat: add extension method

---
+++ Trimly.Core.Application/AddApplication.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Trimly.Core.Application
+{
+    public static class AddApplication
+    {
+        public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
