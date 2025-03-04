67f1ebdbe23ebeea718b0a11f81d03366ce8a0d9
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-03-04 14:38:56 -0400

    feat(logging): add Serilog configuration in Program.cs

--- Trimly/Program.cs
+++ Trimly/Program.cs
@@ -1,34 +1,51 @@
+using Serilog;
 using Trimly.Infrastructure.Persistence;
 using Trimly.Infrastructure.Shared;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var config = builder.Configuration;
-
-builder.Services.AddPersistenceMethod(config);
-builder.Services.AddSharedLayer(config);
-
-var app = builder.Build();
-
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("starting server");
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var config = builder.Configuration;
+    builder.Host.UseSerilog((context, loggerConfiguration) =>
+    {
+        loggerConfiguration.ReadFrom.Configuration(context.Configuration);
+    });
+
+    builder.Services.AddPersistenceMethod(config);
+    builder.Services.AddSharedLayer(config);
+
+    var app = builder.Build();
+    app.UseSerilogRequestLogging();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "server terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
