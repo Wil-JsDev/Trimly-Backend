48d2bc8d39095934c482451c02a225aa10940d46
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-03-03 21:47:33 -0400

    feat: add interfaces for Cloudinary and email services

---
+++ Trimly.Core.Application/Interfaces/Service/ICloudinaryService.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Trimly.Core.Application.Interfaces.Service
+{
+    public interface ICloudinaryService
+    {
+        Task<string> UploadImageCloudinaryAsync(
+           Stream fileStream,
+           string imageName,
+           CancellationToken cancellationToken);
+    }
+}
