52e0a459c1879ce124c57671f6957095df7d18f2
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-03-03 21:49:19 -0400

    feat: implement services

---
+++ Trimly.Infrastructure.Shared/Service/CloudinaryService.cs
@@ -0,0 +1,33 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.Extensions.Options;
+using Trimly.Core.Application.Interfaces.Service;
+using Trimly.Core.Domain.Settings;
+
+namespace Trimly.Infrastructure.Shared.Service
+{
+    public class CloudinaryService : ICloudinaryService
+    {
+        private CloudinarySettings _cloudinarySettings { get; }
+
+        public CloudinaryService(IOptions<CloudinarySettings> cloudinarySettings)
+        {
+            _cloudinarySettings = cloudinarySettings.Value;
+        }
+
+        public async Task<string> UploadImageCloudinaryAsync(Stream fileStream, string imageName, CancellationToken cancellationToken)
+        {
+            Cloudinary cloudinary = new Cloudinary(_cloudinarySettings.CloudinaryUrl);
+            ImageUploadParams image = new()
+            {
+                File = new FileDescription(imageName,fileStream),
+                UseFilename = true,
+                UniqueFilename = false,
+                Overwrite = true
+            };
+
+            var uploadResult = await cloudinary.UploadAsync(image,cancellationToken);
+            return uploadResult.SecureUrl.ToString();
+        }
+    }
+}
