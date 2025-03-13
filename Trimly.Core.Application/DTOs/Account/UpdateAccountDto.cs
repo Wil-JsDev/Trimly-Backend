5de1149adde2bff9561e37bd8f1de3566eff052a
Dayron <dayronbp06@gmail.com>
2025-03-12 21:42:33 -0400

    feat: add-account-service

---
+++ Trimly.Core.Application/DTOs/Account/UpdateAccountDto.cs
@@ -0,0 +1,8 @@
+namespace Trimly.Core.Application.DTOs.Account;
+
+public class UpdateAccountDto
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Username { get; set; }
+}
