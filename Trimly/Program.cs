660f89e395d6c1082416479de893278a331b6f10
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-03-15 15:20:53 -0400

    feat: add API versioning, Swagger, exception handling, and validation services

--- Trimly/Program.cs
+++ Trimly/Program.cs
@@ -1,14 +1,23 @@
 using Serilog;
+using Trimly.Core.Application;
 using Trimly.Infrastructure.Persistence;
 using Trimly.Infrastructure.Shared;
 using Trimly.Infrastructure.Identity;
 using Trimly.Infrastructure.Identity.Seeds;
 using Trimly.Presentation.BackgroundService;
+using Trimly.Presentation.ServiceExtension;
+using Microsoft.Extensions.DependencyInjection;
+using Trimly.Presentation.Extensions;
 
 try
 {
     Log.Information("starting server");
     var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((context, loggerConfiguration) =>
+    {
+        loggerConfiguration.ReadFrom.Configuration(context.Configuration);
+    });
 
     // Add services to the container.
 
@@ -16,18 +25,23 @@ try
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
-    builder.Services.AddHostedService<ConfirmAppointmentBackgroundService>();
+    //builder.Services.AddScoped<IHostedService, ConfirmAppointmentBackgroundService>();
+    //builder.Services.AddHostedService<ConfirmAppointmentBackgroundService>();
     var config = builder.Configuration;
-    builder.Host.UseSerilog((context, loggerConfiguration) =>
-    {
-        loggerConfiguration.ReadFrom.Configuration(context.Configuration);
-    });
 
     builder.Services.AddPersistenceMethod(config);
     builder.Services.AddSharedLayer(config);
     builder.Services.AddIdentityLayer(config);
-
+    builder.Services.AddApplicationLayer();
+    
+    //Extensions
+    builder.Services.AddVersioning();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddException();
+    builder.Services.AddValidation();
+    
     var app = builder.Build();
+    app.UseExceptionHandler(_ => { });
     await app.Services.SeedDatabaseAsync();
     app.UseSerilogRequestLogging();
 
@@ -42,6 +56,8 @@ try
 
     app.UseAuthorization();
 
+    app.UseSwaggerExtension();
+    
     app.MapControllers();
 
     app.Run();
