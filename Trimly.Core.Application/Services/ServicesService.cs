496e6f7f0bfd106adb1e3517d3f04e14bb307847
Dayron <dayronbp06@gmail.com>
2025-03-19 00:04:37 -0400

    fix: remove generate discount code and apply discount code

--- Trimly.Core.Application/Services/ServicesService.cs
+++ Trimly.Core.Application/Services/ServicesService.cs
@@ -202,41 +202,6 @@ public class ServicesService : IServicesService
         return ResultT<Guid>.Success(service.ServicesId ?? Guid.Empty);
     }
 
-    // string return
-    public async Task<ResultT<Guid>> ApplyDiscountCodeAsync(Guid serviceId, Guid registeredCompanyId, string discountCode, CancellationToken cancellationToken)
-    {
-
-        var service = await _repository.GetByIdAsync(serviceId, cancellationToken);
-        if (service == null)
-        {
-            _logger.LogError("Service with ID {ServiceId} not found.", serviceId);
-            
-            return ResultT<Guid>.Failure(Error.NotFound("404", $"{serviceId} Service not found."));
-        }
-        
-        var company = await _registeredCompanyRepository.GetByIdAsync(registeredCompanyId, cancellationToken);
-        if (company == null)
-        {
-            _logger.LogError("Company with ID {CompanyId} not found.", registeredCompanyId);
-            
-            return ResultT<Guid>.Failure(Error.NotFound("404", $"{registeredCompanyId} not found."));
-        }
-
-        if (string.IsNullOrWhiteSpace(discountCode))
-        {
-            _logger.LogError("Invalid discount code provided. Discount code cannot be null or empty.");
-            
-            return ResultT<Guid>.Failure(Error.Failure("400", $"The discount code {discountCode} is invalid."));
-        } 
-        
-        await _repository.ApplyDiscountCodeAsync(service, registeredCompanyId, discountCode, cancellationToken);
-        
-        _logger.LogInformation("Successfully applied discount code {DiscountCode} to service ID {ServiceId} for company ID {CompanyId}.", 
-            discountCode, serviceId, registeredCompanyId);
-        
-        return ResultT<Guid>.Success(service.ServicesId ?? Guid.Empty);
-    }
-
     public async Task<ResultT<IEnumerable<ServiceFilterDTos>>> GetServicesWithDurationLessThan30MinutesAsync(Guid registeredCompany, CancellationToken cancellationToken)
     {
         var company = await _registeredCompanyRepository.GetByIdAsync(registeredCompany, cancellationToken);
@@ -378,4 +343,5 @@ public class ServicesService : IServicesService
 
         return ResultT<IEnumerable<ServiceFilterDTos>>.Success(serviceFilterDTos);
     }
+    
 }
